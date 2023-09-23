759c48e777bf04ad5c0effd65ad55f404ee12e18
nataliiasolomchak21 <>
2023-09-23 17:52:56 +0000

    Add style function for welcome message

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import time
 
 
 SCOPE = [
@@ -42,3 +43,26 @@ while True:
         print(e)
 
 print("Welcome to the Zodiac Sign App, " + username + "!")
+
+
+def style(text, delay=0.008):
+    """
+    Prints out the given text with a delay between each character.
+    """
+    for char in text:
+        print(char, end="", flush=True)
+        time.sleep(delay)
+    print()
+
+
+style(
+    """                                                        
+                               88 88                        
+                               88 ""                        
+                               88                           
+888888888  ,adPPYba,   ,adPPYb,88 88 ,adPPYYba,  ,adPPYba,  
+     a8P" a8"     "8a a8"    `Y88 88 ""     `Y8 a8"     ""  
+  ,d8P'   8b       d8 8b       88 88 ,adPPPPP88 8b          
+,d8"      "8a,   ,a8" "8a,   ,d88 88 88,    ,88 "8a,   ,aa  
+888888888  `"YbbdP"'   `"8bbdP"Y8 88 `"8bbdP"Y8  `"Ybbd8"'  """
+)

21972d0edf735743dc6cdddf0d62483d5eaecae6
nataliiasolomchak21 <>
2023-09-23 17:38:12 +0000

    Add a dictionary with descriptions

---
+++ descriptions.py
@@ -0,0 +1,14 @@
+descriptions = {
+    'Aries': ['courageous,', 'enthusiastic,', 'and impulsive'],
+    'Taurus': ['reliable,', 'patient,', 'and stubborn'],
+    'Gemini': ['adaptable,', 'versatile,', 'and restless'],
+    'Cancer': ['intuitive,', 'emotional,', ' and protective'],
+    'Leo': ['confident,', 'ambitious,', 'and domineering'],
+    'Virgo': ['analytical,', 'observant,', 'and critical'],
+    'Libra': ['diplomatic,', 'graceful,', 'and indecisive'],
+    'Scorpio': ['passionate,', 'tenacious,', 'and jealous'],
+    'Sagittarius': ['optimistic,', 'adventurous,', 'and careless'],
+    'Capricorn': ['practical,', 'disciplined,', 'and pessimistic'],
+    'Aquarius': ['progressive,', 'original,', 'and unpredictable'],
+    'Pisces': ['compassionate,', 'sensitive,', 'and escapist']
+}

03aaadba30b877d1b72ff135ec712e7bdf13dacc
nataliiasolomchak21 <>
2023-09-23 18:02:48 +0000

    Add a function to get user's zodiac sign

---
+++ signs.py
@@ -0,0 +1,60 @@
+from descriptions import descriptions
+
+
+def get_zodiac_sign(birth_day, birth_month):
+    """
+    Determines and prints the zodiac sign based on a given birth day and month.
+    Checks the birth day and month against the date ranges for each zodiac sign.
+    Prints the sign, symbol, and traits if a match is found.
+    """
+    if (birth_month == 12 and birth_day >= 22) or (
+        birth_month == 1 and birth_day <= 19
+    ):
+        sign = "Capricorn"
+    elif (birth_month == 1 and birth_day >= 20) or (
+        birth_month == 2 and birth_day <= 18
+    ):
+        sign = "Aquarius"
+    elif (birth_month == 2 and birth_day >= 19) or (
+        birth_month == 3 and birth_day <= 20
+    ):
+        sign = "Pisces"
+    elif (birth_month == 3 and birth_day >= 21) or (
+        birth_month == 4 and birth_day <= 19
+    ):
+        sign = "Aries"
+    elif (birth_month == 4 and birth_day >= 20) or (
+        birth_month == 5 and birth_day <= 20
+    ):
+        sign = "Taurus"
+    elif (birth_month == 5 and birth_day >= 21) or (
+        birth_month == 6 and birth_day <= 20
+    ):
+        sign = "Gemini"
+    elif (birth_month == 6 and birth_day >= 21) or (
+        birth_month == 7 and birth_day <= 22
+    ):
+        sign = "Cancer"
+    elif (birth_month == 7 and birth_day >= 23) or (
+        birth_month == 8 and birth_day <= 22
+    ):
+        sign = "Leo"
+    elif (birth_month == 8 and birth_day >= 23) or (
+        birth_month == 9 and birth_day <= 22
+    ):
+        sign = "Virgo"
+    elif (birth_month == 9 and birth_day >= 23) or (
+        birth_month == 10 and birth_day <= 22
+    ):
+        sign = "Libra"
+    elif (birth_month == 10 and birth_day >= 23) or (
+        birth_month == 11 and birth_day <= 21
+    ):
+        sign = "Scorpio"
+    elif (birth_month == 11 and birth_day >= 22) or (
+        birth_month == 12 and birth_day <= 21
+    ):
+        sign = "Sagittarius"
+
+    print("Your zodiac sign is " + sign)
+    print(f"You are: {' '.join(descriptions[sign])}")

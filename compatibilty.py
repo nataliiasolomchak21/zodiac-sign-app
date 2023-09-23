a4242a8db04e0716c8461a6369f543384c84739f
nataliiasolomchak21 <>
2023-09-23 17:36:17 +0000

    Add a dictionary for compatibility

---
+++ compatibilty.py
@@ -0,0 +1,14 @@
+zodiac_compatibility = {
+    'Aries': ['Leo', 'Sagittarius', 'Gemini', 'Aquarius', 'Libra', 'Virgo'],
+    'Taurus': ['Virgo', 'Capricorn', 'Cancer', 'Pisces', 'Scorpio', 'Taurus'],
+    'Gemini': ['Libra', 'Aquarius', 'Aries', 'Sagittarius', 'Cancer', 'Leo'],
+    'Cancer': ['Scorpio', 'Pisces', 'Taurus', 'Virgo', 'Leo', 'Capricorn'],
+    'Leo': ['Libra', 'Sagittarius', 'Gemini', 'Aries', 'Capricorn', 'Cancer'],
+    'Virgo': ['Taurus', 'Capricorn', 'Cancer', 'Scorpio', 'Virgo', 'Pisces'],
+    'Libra': ['Gemini', 'Aquarius', 'Taurus', 'Sagittarius', 'Leo', 'Virgo'],
+    'Scorpio': ['Cancer', 'Pisces', 'Capricorn', 'Virgo', 'Taurus', 'Scorpio'],
+    'Sagittarius': ['Aries', 'Pisces', 'Gemini', 'Libra', 'Leo', 'Virgo'],
+    'Capricorn': ['Taurus', 'Virgo', 'Scorpio', 'Pisces', 'Aries', 'Libra'],
+    'Aquarius': ['Gemini', 'Libra', 'Aquarius', 'Sagittarius', 'Aries', 'Leo'],
+    'Pisces': ['Cancer', 'Scorpio', 'Taurus', 'Capricorn', 'Virgo', 'Libra']
+}
